a43a8d26fefd3e4bbd64ca1224d791f4e8850cb5
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-11-30 14:22:52 +0545

    feat: ball are drawn

--- src/scripts/Ball.js
+++ src/scripts/Ball.js
@@ -1,4 +1,9 @@
 class Ball {
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
     constructor(x, y){
         this.x = x;
         this.y = y;
@@ -6,7 +11,7 @@ class Ball {
      this.element = document.createElement('div');
      this.element.classList.add('ball');
     }
-    }
+   
 
     /**
      * returns the dom 
@@ -15,5 +20,34 @@ class Ball {
      */
 
     getElement = ()=>this.element;
+
+    /**
+     * 
+     */
     getX = ()=>this.x;
+
+    /**
+     * 
+     */
     getY = ()=>this.y;
+
+
+    /**
+     * @param {number} x
+     */
+    SetX = (x)=>{
+        this.x = x;
+    }
+
+    /**
+     * @param {number} y
+     */
+    SetY = (y)=>{
+        this.y = y;
+    }
+
+    draw = ()=>{
+        this.element.style.left = this.x + 'px';
+        this.element.style.top = this.y + 'px';
+    }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,80 +1,28 @@
-// const viewport = document.querySelector(".viewport");
+// balls will be rendered in this dom element
+const container = document.querySelector(".container");
 
-// const ballArray = [];
+// Array to store balls
+const ballsArray = [];
 
-// for (let i=0; i<Ball_COUNT; i++){
-//     const ball = new Ball(100, 100);
-//     ballArray.push(ball);
-// }
-
-
-
-
-
-
-
-
-// let speed = 1;
-// const position = {
-//     x : 200,
-//     y : 10
-// };
-
-const container = document.querySelector('.container');
-
-
-
-
-// const checkCollision = ()=>{
-//     if (position.x > 500-20 || position.x <0){
-//         speed *= -1;
-//     }
-// }
-
-// const update = ()=>{
-//     position.x += speed;
-// }
-
-// const animate = ()=>{
-//     update();
-//     draw();
-//     checkCollision();
-//     window.requestAnimationFrame(animate);
-// }
-// animate();
-
-const randomGen = (lower, upper)=>{
-    return lower + Math.random() * (upper - lower);
+// create balls and store in an array
+for (let i=0; i<Ball_COUNT; i++){
+    const x = getRandom(0, VIEWPORT_WIDTH-BALL_WIDTH)
+    const y = getRandom(0, VIEWPORT_HEIGHT-BALL_HEIGHT)
+    const ball = new Ball(x, y);
+    ballsArray.push(ball);
 }
 
-class Ball {
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-
-     this.element = document.createElement('div');
-     this.element.classList.add('ball');
-    }
-
-    draw = ()=>{
-        this.element.style.top = this.x + 'px';
-        this.element.style.left = this.y + 'px';
-        container.appendChild(this.element);
-    }
-}
-
-let points = [];
-
-for (let i = 0; i<20; i++){
-    let x = randomGen(0, 480);
-    let y = randomGen(0, 480);
-    points.push({x, y});
-}
-console.log(points);
-points.forEach((data, index)=>{
-    let ball = new Ball(data.x, data.y);
-    ball.draw();
+// add balls in viewport
+ballsArray.forEach(ball =>{
+    container.appendChild(ball.getElement());
 });
 
 
+const render = ()=>{
+    ballsArray.forEach((ball)=>{
+        ball.draw();
+    })
+    requestAnimationFrame(render);
+}
 
+render();
